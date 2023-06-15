ccb948c345b9091936257ff4d574400830db69c1
Nikita Kryuchkov <black.unik.06@gmail.com>
2023-06-15 16:53:04 +0300

    change file

--- laba.py
+++ laba.py
@@ -1,18 +1,16 @@
-graph = {
-    '1': ['3'],
-    '2': ['4'],
-    '3': [],
-    '4': ['2']
-}
+def dfs(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return len(path)-1
+    for node in graph[start]:
+        if node not in path:
+            newpath = dfs(graph, node, end, path)
+            if newpath is not None:
+                return newpath
 
-def dfs(vertex, visited):
-    visited.add(vertex)
-    print(vertex, end=" ")
-    for neighbor in graph[vertex]:
-        if neighbor not in visited:
-            dfs(neighbor, visited)
 
-start_vertex = '1'
-visited_vertices = set()
 
-dfs(start_vertex, visited_vertices)
+graph = {1: [2,3], 2: [1,4], 3: [1], 4: [2]}
+start = 2
+end = 4
+print(dfs(graph, start, end))
