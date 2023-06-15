3d8cb4bd904dd5946c2700dd12a13092ceeef7bb
Nikita Kryuchkov <black.unik.06@gmail.com>
2023-06-15 17:28:31 +0300

    more comments

--- laba.py
+++ laba.py
@@ -1,16 +1,40 @@
-def dfs(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return len(path)-1
-    for node in graph[start]:
-        if node not in path:
-            newpath = dfs(graph, node, end, path)
-            if newpath is not None:
-                return newpath
+#комментарии
+#Создаем граф
+graph = {
+    'a': ['b','c'],
+    'b': ['d','e'],
+    'c': ['f'],
+    'd': [],
+    'e': ['f'],
+    'f': []
+}
 
+#Реализуем функцию обхода графа в глубину с проверкой на некорректные знавчения входных параметров
+def dfs_path(graph, current_vertex, target_vertex, visited=[]):
+    visited.append(current_vertex)
 
+    if current_vertex==target_vertex: #комментарии1
+        return visited
 
-graph = {1: [2,3], 2: [1,4], 3: [1], 4: [2]}
-start = 2
-end = 4
-print(dfs(graph, start, end))
+    if current_vertex not in graph:
+        raise ValueError(f"Вершина {current_vertex} не найдена в графе")
+
+    for neighbor in graph[current_vertex]: #комментарии2
+        if neighbor not in visited:
+            path = dfs_path(graph, neighbor, target_vertex, visited)
+            if path:
+                return path #комментарии3
+
+    return None
+
+#Вызываем функцию для проверки
+try:
+    a='a'
+    b='e'
+    path=dfs_path(graph, a, b) #комментарии4
+    if path:
+        print(f"Длина пути от вершины {a} до вершины {b} составляет {len(path) - 1} штрихов")
+    else:
+        print(f"Путь от вершины {a} до вершины {b} не найден")
+except ValueError as e:
+    print(str(e))
